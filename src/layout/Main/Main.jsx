3cfe30f7c5ba030b186881b63c123d4f7a7ebd59
Shanta Islam <shantoislam7363@gmail.com>
2023-11-24 22:02:35 +0600

    Footer is added in home page

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../../shared/Footer";
 
 
 const Main = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
