786c840cbd7604287b804519023880aa9688cbc5
Shanta Islam <shantoislam7363@gmail.com>
2023-11-24 21:28:56 +0600

    Initial setup in client side done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Medical Camp</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/shared/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+            <h2>THis is footer</h2>
+        </div>
+    );
+};
+
+export default Footer;
